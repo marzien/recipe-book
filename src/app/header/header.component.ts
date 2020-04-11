ce327bd09c5a9a537cd2ff01628cbe19e5d8a2cd
Marius Zienius <marius.zienius@gmail.com>
2020-04-11 13:33:22 +0200

    Passing Data with Event and Property Binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,6 @@ export class HeaderComponent {
   @Output() featureSelected = new EventEmitter<string>();
 
   onSelect(feature: string) {
-    this.featureSelected.emit(feature)
+    this.featureSelected.emit(feature);
   }
 }
