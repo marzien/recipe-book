5ffdb7bb6eafcc99f61d01af8ad8d342dd7a821b
Marius Zienius <marius.zienius@gmail.com>
2020-04-17 16:12:30 +0200

    transforming response data and empty array of ingredients if there is no ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,25 @@ import { Subject } from 'rxjs/internal/Subject';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Vienna Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://www.viennawurstelstand.com/wp-content/uploads/2017/03/viennawurstelstand_bestschnitzel_header2.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20),
-      ]),
-    new Recipe(
-      'Big Burger',
-      'The Tastiest Burger in City',
-      'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/burger4_0.png?itok=bu8uibsO&resize=1100x1100',
-      [
-        new Ingredient('Buns', 3),
-        new Ingredient('Meat', 1),
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Vienna Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://www.viennawurstelstand.com/wp-content/uploads/2017/03/viennawurstelstand_bestschnitzel_header2.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20),
+  //     ]),
+  //   new Recipe(
+  //     'Big Burger',
+  //     'The Tastiest Burger in City',
+  //     'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/burger4_0.png?itok=bu8uibsO&resize=1100x1100',
+  //     [
+  //       new Ingredient('Buns', 3),
+  //       new Ingredient('Meat', 1),
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slcService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -22,6 +23,11 @@ export class DataStorageService {
   fetchRecipes() {
     this.http.get<Recipe[]>(
       'https://ng-recipe-book-3e4b5.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
