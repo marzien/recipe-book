f23c1e215d906ee3d49df1a66f870313e1d12860
Marius Zienius <marius.zienius@gmail.com>
2020-04-20 23:14:57 +0200

    fix project naming to recipe-book

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-project app is running!');
+    expect(page.getTitleText()).toEqual('recipe-book app is running!');
   });
 
   afterEach(async () => {
